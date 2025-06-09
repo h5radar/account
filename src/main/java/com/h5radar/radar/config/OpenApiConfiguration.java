dd146a14ea896d8b1f5c3038a946800e1482bbcb
arufanov <arufanov@gmail.com>
2025-06-09 13:25:24 +0300

    The initial commit

---
+++ src/main/java/com/h5radar/radar/config/OpenApiConfiguration.java
@@ -0,0 +1,38 @@
+package com.h5radar.radar.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class OpenApiConfiguration implements WebMvcConfigurer {
+
+  private SecurityScheme createApiKeyScheme() {
+    return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+        .bearerFormat("JWT")
+        .scheme("bearer");
+  }
+
+  @Bean
+  public OpenAPI openApi() {
+    return new OpenAPI().addSecurityItem(new SecurityRequirement()
+            .addList("Bearer Authentication"))
+        .components(new Components()
+            .addSecuritySchemes("Bearer Authentication", createApiKeyScheme()));
+  }
+
+  /* TODO:
+  @Bean
+  public OpenApiCustomiser sortSchemasAlphabetically() {
+    return openApi -> {
+      Map<String, Schema> schemas = openApi.getComponents().getSchemas();
+      openApi.getComponents().setSchemas(new TreeMap<>(schemas));
+    };
+  }
+  */
+}
---
+++ src/main/java/com/h5radar/radar/config/SecurityConfiguration.java
@@ -0,0 +1,59 @@
+package com.h5radar.radar.config;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+  @Value("${application.cors.allowed-origins}")
+  String allowedOrigins;
+
+  @Value("${application.cors.allowed-methods}")
+  String allowedMethods;
+
+  @Value("${application.cors.allowed-headers}")
+  String allowedHeaders;
+
+  @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+  String jwkSetUri;
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .cors(cors -> cors.configurationSource(request -> {
+          CorsConfiguration configuration = new CorsConfiguration();
+          configuration.setAllowedOrigins(Arrays.asList(this.allowedOrigins));
+          configuration.setAllowedMethods(Arrays.asList(this.allowedMethods));
+          configuration.setAllowedHeaders(Arrays.asList(this.allowedHeaders));
+          return configuration;
+        }))
+        .authorizeHttpRequests((requests) -> requests
+            .requestMatchers("/actuator/**").permitAll()
+            .requestMatchers("/api/v1/application/**").permitAll()
+            .requestMatchers("/swagger-ui/**").permitAll()
+            .requestMatchers("/v3/api-docs/**").permitAll()
+            .anyRequest().authenticated()
+        )
+        .oauth2ResourceServer((oauth2) -> oauth2
+            .jwt(withDefaults()));
+    return http.build();
+  }
+
+  @Bean
+  JwtDecoder jwtDecoder() {
+    return NimbusJwtDecoder.withJwkSetUri(this.jwkSetUri).build();
+  }
+}
